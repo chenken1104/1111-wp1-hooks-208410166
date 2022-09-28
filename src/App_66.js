64da3f42a07d349b06848820e3409eb35f8f7268
chenken1104 <a0976504007@gmail.com>
2022-09-28 20:09:20 +0800

    W04-P2: Hooks Tutorial T12_66, fix problem in T11_66

--- src/App_66.js
+++ src/App_66.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import T11_66 from './tutorial/1-useState/T11_66-error-example';
+import T12_66 from './tutorial/1-useState/T12_66-useState-basics';
 
 const App_66 = () => {
   return (
     <div className='container'>
       <h2>Tutorial 11 - 208410166</h2>
-      <T11_66 />
+      <T12_66 />
     </div>
   );
 };
