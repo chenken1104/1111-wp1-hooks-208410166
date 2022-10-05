2a2235c064163ff2b9cdbd2186bde795d94f6b60
chenken1104 <a0976504007@gmail.com>
2022-10-05 20:19:22 +0800

    T23_66: fetch data from Github and display them

--- src/App_66.js
+++ src/App_66.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import T21_66 from './tutorial/2-useEffect/T21_66-useEffect-basics';
+import T23_66 from './tutorial/2-useEffect/T23_66-useEffect-fetch-data';
 
 const App_66 = () => {
   return (
     <div className='container'>
-      <h2>Tutorial 21 - 208410166</h2>
-      <T21_66 />
+      <h2>Tutorial 23 - 208410166</h2>
+      <T23_66 />
     </div>
   );
 };
