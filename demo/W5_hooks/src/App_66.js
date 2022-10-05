e75889589b31a0cca42878d9b0453bf63aaa5c99
chenken1104 <a0976504007@gmail.com>
2022-10-05 21:41:39 +0800

    ned

---
+++ demo/W5_hooks/src/App_66.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import T31_66 from './tutorial/3-conditional-rendering/T31_66-multiple-returns';
+
+const App_66 = () => {
+  return (
+    <div className='container'>
+      <h2>Tutorial 31 - 208410166</h2>
+      <T31_66 />
+    </div>
+  );
+};
+
+export default App_66;
