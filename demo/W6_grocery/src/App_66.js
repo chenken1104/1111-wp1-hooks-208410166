3ba2e9d52a9ff3bfa5496da71f13c02a818cd98f
chenken1104 <chenken1104@gmail.com>
2022-10-12 21:31:04 +0800

    end demo

---
+++ demo/W6_grocery/src/App_66.js
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from 'react'
+import List_66 from './components/List_66'
+import Alert_66 from './components/Alert_66'
+
+const getLocalStorage = () => {
+  let list = localStorage.getItem('list');
+  if(list) {
+    return JSON.parse(localStorage.getItem('list'));
+  } else {
+    return [];
+  }
+}
+
+const App_66 = () => {
+  const [name, setName] = useState('');
+  const [list, setList] = useState(getLocalStorage());
+  const [alert, setAlert] = useState({
+    show: false,
+    msg: '',
+    type: '',
+  });
+
+  useEffect(() => {
+    localStorage.setItem('list', JSON.stringify(list));
+  }, [list]);
+
+  const showAlert = (show = false, msg = '', type= '') => {
+    setAlert({ show, msg, type });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name){
+      showAlert(true, 'please enter value', 'danger');
+    } else {
+      showAlert(true, 'value changed', 'success');
+      const newItem = {
+        id: new Date().getTime().toString(),
+        title: name,
+      };
+      setList([...list, newItem]);
+      setName('');
+    };
+  };
+
+  const removeItem = (id) => {
+    showAlert(true, 'item removed', 'danger');
+    setList(list.filter((item) => item.id !== id));
+  }
+
+  const clearList = () => {
+    showAlert(true, 'empty list', 'danger');
+    setList([]);
+  };
+
+  return (
+  <>
+    <section className='section-center'>
+      <form className='grocery-form' onSubmit={handleSubmit}>
+        { alert.show && <Alert_66 {...alert} removeAlert={showAlert} />}
+        <h3>Grocery Bud - 208410166</h3>
+        <div className='form-control'>
+          <input 
+            type='text'
+            className='grocery'
+            placeholder='e.g. eggs' 
+            value={name} 
+            onChange={(e) => {
+              setName(e.target.value)
+            }} />
+          <button type='submit' className='submit-btn'>submit</button>
+        </div>
+      </form>
+      { list.length > 0 && (
+        <div className='grocery-container'>
+          <List_66 items={list} removeItem={removeItem} />
+          <button className='clear-btn' onClick={clearList}>clear items</button>
+        </div>
+      ) }
+    </section>
+  </>
+  )
+}
+
+export default App_66
